039c658eec805acf75b9772c8b45752f2f12f5ca
minseo0614 <125117397+minseo0614@users.noreply.github.com>
2025-08-28 22:23:10 +0900

    Create MouseAdapterEx.java

---
+++ chap09_eventhandling/MouseAdapterEx.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class MouseAdapterEx extends JFrame {
+    private JLabel la = new JLabel("Hello");
+    public MouseAdapterEx() {
+        setTitle("Mouse 이벤트 예제");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container c = getContentPane();
+        c.setLayout(null);
+        la.setSize(50, 20);
+        la.setLocation(30, 30);
+        c.add(la);
+        c.addMouseListener(new MyMouseAdapter());
+        setSize(200, 200);
+        setVisible(true);
+    }
+    class MyMouseAdapter extends MouseAdapter {
+        public void mousePressed(MouseEvent e) { la.setLocation(e.getX(), e.getY()); }
+    }
+    public static void main(String[] args) { new MouseAdapterEx(); }
+}
